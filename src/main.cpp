0f07b2fb089cd374555d69bc610bc2ad29685bbb
Lunar-Eclipse255 <neptunesaturn255@gmail.com>
2024-09-07 18:07:08 -0400

    stuff

--- src/main.cpp
+++ src/main.cpp
@@ -31,9 +31,9 @@ void on_center_button() {
  * to keep execution time for this mode under a few seconds.
  */
 void initialize() {
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
-	pros::lcd::register_btn1_cb(on_center_button);
+	// pros::lcd::initialize();
+	// pros::lcd::set_text(1, "Hello PROS User!");
+	// pros::lcd::register_btn1_cb(on_center_button);
 	
 	bool* temp = maelstrom::logging::init(true, true, left_motors, right_motors, 50);
 	file_created[0] = temp[0];
